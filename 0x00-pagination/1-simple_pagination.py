f52a43d590423245be7dc2afc0ae539664990ef8
Lafertd <43250553+Lafertd@users.noreply.github.com>
2024-11-03 00:34:55 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -7,11 +7,11 @@ from typing import List
 def index_range(page: int, page_size: int) -> tuple:
     """
     Calculate the start and end indices for the items in a given page
-    
+
     Args:
     page (int): The 1-indexed number of the page
     page_size (int): The number of items in a page
-    
+
     Returns:
     tuple: A tuple containing the start and the end of the index range
     """
@@ -33,9 +33,9 @@ class Server:
 
     def dataset(self) -> List[List]:
         """Cached dataset
-        
+
         Returns:
-        List[List]: The dataset loaded from the CSV file, excluding the header row.
+        List[List]: The dataset except the header row
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -44,25 +44,25 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-    
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Get a page from the dataset
-        
+
         Args:
         page (int): The 1-indexed number of the page
         page_size (int): The number of items in a page
-        
+
         Returns:
         List[List]: A list of rows corresponding to the requested page
         """
-       
+
         data_object = Server.dataset(self)
-        assert type(page) == int and type(page_size) == int
+        assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         interval = index_range(page, page_size)
         if interval[0] >= len(data_object) or interval[1] < 0:
             return []
-        
+
         result = [row for row in data_object[interval[0]: interval[1]]]
         return result
