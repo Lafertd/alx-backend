1dd7b29aca1b9788b893e38160c74383483a9c32
Lafertd <43250553+Lafertd@users.noreply.github.com>
2024-11-02 02:04:43 +0100

    up

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python3
-"""Task 1: Simple pagination.
-"""
-
 import csv
 import math
-from typing import List, Tuple
+from typing import List
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
-    """
+def index_range(page: int, page_size: int) -> tuple:
+         """
+         Calculate the start and end indices for the items in a given page
+         
+         Args:
+         page (int): The 1-indexed number of the page
+         page_size (int): The number of items in a page
+         
+         Returns:
+         tuple: A tuple containing the start and the end of the index range
+        """
+         end = page * page_size
+         start = page_size * (page - 1)
+         return (start, end)
 
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+
 
 
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-    DATA_FILE = "Popular_Baby_Names.csv"
 
+    DATA_FILE = "Popular_Baby_Names.csv"
+    index_range = index_range
     def __init__(self):
         self.__dataset = None
 
@@ -32,14 +41,15 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-
+    
+    
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
-        """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
-            return []
-        return data[start:end]
+            """
+            """
+            data_object = Server.dataset(self)
+            assert type(page) == int and type(page_size) == int
+            assert page > 0 and page_size > 0
+            interval = index_range(page, page_size)
+            
+            result = [row for row in data_object[interval[0]: interval[1]]]
+            return result
