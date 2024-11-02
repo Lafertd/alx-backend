7ba939d8bffce4cb45ba94fff9976ae21e636b98
Lafertd <43250553+Lafertd@users.noreply.github.com>
2024-11-03 00:42:28 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
-"""Task 1: Simple pagination.
-"""
-
 import csv
 import math
-from typing import List, Tuple
+from typing import List
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
+def index_range(page: int, page_size: int) -> tuple:
     """
+    Calculate the start and end indices for the items in a given page
 
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+    Args:
+    page (int): The 1-indexed number of the page
+    page_size (int): The number of items in a page
+
+    Returns:
+    tuple: A tuple containing the start and the end of the index range
+    """
+    end = page * page_size
+    start = page_size * (page - 1)
+    return (start, end)
 
 
 class Server:
     """Server class to paginate a database of popular baby names.
     """
+
     DATA_FILE = "Popular_Baby_Names.csv"
+    index_range = index_range
 
     def __init__(self):
+        """Initialize the Server instance."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset
+
+        Returns:
+        List[List]: The dataset except the header row
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -34,13 +46,23 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
         """
-        assert type(page) == int and type(page_size) == int
+        Get a page from the dataset
+
+        Args:
+        page (int): The 1-indexed number of the page
+        page_size (int): The number of items in a page
+
+        Returns:
+        List[List]: A list of rows corresponding to the requested page
+        """
+
+        data_object = Server.dataset(self)
+        assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
+        interval = index_range(page, page_size)
+        if interval[0] >= len(data_object) or interval[0] < 0:
             return []
-        return data[start:end]
-    
+
+        result = [row for row in data_object[interval[0]: interval[1]]]
+        return result
