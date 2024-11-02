7cf0aeb2d014fd6c72389ef738cee083c0c5ceab
Lafertd <43250553+Lafertd@users.noreply.github.com>
2024-11-02 23:02:23 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,23 +1,24 @@
-#!/usr/bin/env python3
 import csv
 import math
 from typing import List
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """
+         """
          Calculate the start and end indices for the items in a given page
-
+         
          Args:
          page (int): The 1-indexed number of the page
          page_size (int): The number of items in a page
-
+         
          Returns:
          tuple: A tuple containing the start and the end of the index range
-    """
-    end = page * page_size
-    start = page_size * (page - 1)
-    return (start, end)
+        """
+         end = page * page_size
+         start = page_size * (page - 1)
+         return (start, end)
+
+
 
 
 class Server:
@@ -26,7 +27,6 @@ class Server:
 
     DATA_FILE = "Popular_Baby_Names.csv"
     index_range = index_range
-
     def __init__(self):
         self.__dataset = None
 
@@ -39,16 +39,16 @@ class Server:
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
 
-            return self.__dataset
-
+        return self.__dataset
+    
+    
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        """
-        if 
-        data_object = Server.dataset(self)
-        assert isinstance(page, int)
-        assert isinstance(page_size, int)
-        assert page > 0 and page_size > 0
-        interval = index_range(page, page_size)
-        result = [row for row in data_object[interval[0]: interval[1]]]
-        return result
+            """
+            """
+            data_object = Server.dataset(self)
+            assert type(page) == int and type(page_size) == int
+            assert page > 0 and page_size > 0
+            interval = index_range(page, page_size)
+            
+            result = [row for row in data_object[interval[0]: interval[1]]]
+            return result
