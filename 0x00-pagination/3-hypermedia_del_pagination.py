a1b2ccf8f6d04d3672d8f69635a9bef434600f2a
Lafertd <43250553+Lafertd@users.noreply.github.com>
2024-12-05 16:56:35 +0100

    Added the indexed_dataset func

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -13,6 +13,17 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
+def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
